628650f03b68f719678e46e4c7c986d55b21a874
gripefruits <gripefruits@gmail.com>
2020-05-04 15:17:51 -0700

    UPDATE puka packages

--- package/puka-conf/puka-conf.mk
+++ package/puka-conf/puka-conf.mk
@@ -8,7 +8,8 @@ PUKA_CONF_INSTALL_TARGET = YES
 
 define PUKA_CONF_INSTALL_TARGET_CMDS
     $(INSTALL) -D -m 0644 $(PUKA_CONF_PKGDIR)/openssl.cnf $(TARGET_DIR)/etc/ssl/openssl.cnf
-    $(INSTALL) -D -m 0644 $(PUKA_CONF_PKGDIR)/puka-engine.cnf $(TARGET_DIR)/etc/ssl/puka-engine.cnf
+    $(INSTALL) -D -m 0644 $(PUKA_CONF_PKGDIR)/puka_engine.cnf $(TARGET_DIR)/etc/ssl/puka-engine.cnf
+    $(INSTALL) -D -m 0644 $(PUKA_CONF_PKGDIR)/profile $(TARGET_DIR)/etc/profile
 endef
 
 $(eval $(generic-package))
--- package/puka-engine/puka-engine.mk
+++ package/puka-engine/puka-engine.mk
@@ -3,7 +3,7 @@
 # puka-engine
 #
 ################################################################################
-PUKA_ENGINE_VERSION = d56c68eb413ddc18db074bdb78bc73d4b4799d49 
+PUKA_ENGINE_VERSION = d56c68eb413ddc18db074bdb78bc73d4b4799d49
 PUKA_ENGINE_SITE_METHOD = git
 PUKA_ENGINE_SITE = ssh://git@github.com/cryptotronix/puka.git
 PUKA_ENGINE_LICENSE = Apache-2.0 GPL-3.0-or-later BSD-4-Clause-UC OpenSSL CC0-1.0 MIT-like
@@ -14,8 +14,9 @@ PUKA_ENGINE_DEPENDENCIES = openssl libopenssl util-linux glibc sqlite python3 li
 PUKA_ENGINE_CONF_ENV = \
 	GPG_ERROR_CONFIG=$(STAGING_DIR)/usr/bin/gpg-error-config
 PUKA_ENGINE_CONF_OPTS += \
-	--with-gpgme-prefix=$(STAGING_DIR)/usr \
-	--with-enginesdir=$(TARGET_DIR)/usr/lib/engines-1.1
+	--with-enginesdir=$(TARGET_DIR)/usr/lib/engines-1.1 \
+	--disable-emulator \
+	--enable-uartlite
 PUKA_ENGINE_AUTORECONF = YES
 
 $(eval $(autotools-package))
